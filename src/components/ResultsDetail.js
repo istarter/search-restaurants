ac0bffdd59de06dd643d1aff484dd832077520fc
Najib Khan <naxibkhan@gmail.com>
2020-04-05 20:55:53 +0500

    Rendering resturants name on home screen

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const ResultsDetail = ({result}) => {
+  return <Text>{result.name}</Text>;
+};
+
+const styles = StyleSheet.create({});
+export default ResultsDetail;
